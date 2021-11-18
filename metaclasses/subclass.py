4e07cc52198bd54b6d88f825bf5e0afe2cf37466
Jochen Keil <1392715+jchnkl@users.noreply.github.com>
2021-11-18 12:31:08 +0100

    Update example

--- metaclasses/subclass.py
+++ metaclasses/subclass.py
@@ -1,34 +1,26 @@
-# a final metaclass. Subclassing a class that has the Final metaclass should fail
+# Eine Nicht-Ableitbare Klasse
 class Final(type):
     def __new__(cls, name, bases, attr):
-        # Final cannot be subclassed
-        # check that a Final class has not been passed as a base
-        # if so, raise error, else, create the new class with Final attributes
+        # Ueberprueft, ob Final bereits in einer Subklasse genutzt wurde. Falls
+        # ja, wird eine Exception geworfen
         type_arr = [type(x) for x in bases]
         for i in type_arr:
             if i is Final:
                 raise RuntimeError("You cannot subclass a Final class")
         return super(Final, cls).__new__(cls, name, bases, attr)
 
-# Test: use the metaclass to create a Cop class that is final
+# Test: Nutzt die Metaklasse um eine nicht weiter vererbare Klasse zu erzeugen
 
 class Cop(metaclass=Final):
     def exit():
         print("Exiting...")
         quit()
 
-# Attempt to subclass the Cop class, this should idealy raise an exception!
+# Der Versuch eine weitere Vererbung vorzunehmen sollte mit einer Fehlermeldung
+# quittiert werden
 class FakeCop(Cop):
     def scam():
         print("This is a hold up!")
 
 cop1 = Cop()
 fakecop1 = FakeCop()
-
-# More tests, another Final class
-class Goat(metaclass=Final):
-    location = "Goatland"
-
-# Subclassing a final class should fail
-class BillyGoat(Goat):
-    location = "Billyland"
