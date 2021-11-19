15881d1f9c14a42904db5ff8b295b81e4bcaeb70
Jochen Keil <1392715+jchnkl@users.noreply.github.com>
2021-11-19 11:25:38 +0100

    Update Exercise 1

--- metaclasses/exercise_1.py
+++ metaclasses/exercise_1.py
@@ -1,4 +1,9 @@
-# ACHTUNG: Funktioniert nur mir Python 2!
+def uppercase(attrs):
+    tmp = dict()
+    for attr, v in attrs.items():
+        if not attr.startswith("__"):
+            tmp[attr.upper()] = v
+    return tmp
 
 # die gleiche Signatur wie `type`
 def upper_attr(class_name, class_bases, class_attrs):
@@ -8,20 +13,26 @@ def upper_attr(class_name, class_bases, class_attrs):
     """
 
     # veraendert jedes Attribut, welches nicht mit '__' startet
-    uppercase_attrs = {
-        attr if attr.startswith("__") else attr.upper(): v
-        for attr, v in class_attrs.items()
-    }
+    # uppercase_attrs = {
+    #     attr if attr.startswith("__") else attr.upper(): v
+    #     for attr, v in class_attrs.items()
+    # }
+    uppercase_attrs = uppercase(class_attrs)
 
     # `type` erzeugt weiterhin die Klasse
+    # Implementierung der Funktionalitaet von `type` in reinem Python nicht
+    # moeglich!
     return type(class_name, class_bases, uppercase_attrs)
 
 # Ab Python 3 funktioniert das modul-weite setzen von __metaclass__ *nicht* mehr
-# wird von allen Klassen in diesem Modul genutzt
+# Python 2: wird von allen Klassen in diesem Modul genutzt
 # __metaclass__ = upper_attr
 
 # Python 2
 # class Foo():
+#     __metaclass__ = upper_attr
+#     pass
+
 # Python 3
 class Foo(object, metaclass=upper_attr):
     bar = 'bip'
