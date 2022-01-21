17cd5ca2cf6b2603519a7e28a51eb4cfe5bedf87
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2022-01-21 10:01:00 +0100

    Create exercise2.sql

---
+++ mysql/exercise2.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Departments (
+    Id INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(25) NOT NULL,
+    PRIMARY KEY(Id)
+);
+
+INSERT INTO Departments
+    (Id, Name)
+VALUES
+    (1, 'HR'),
+    (2, 'Sales'),
+    (3, 'Tech')
+;
+
+CREATE TABLE Employees (
+    Id INT NOT NULL AUTO_INCREMENT,
+    FName VARCHAR(35) NOT NULL,
+    LName VARCHAR(35) NOT NULL,
+    PhoneNumber VARCHAR(11),
+    ManagerId INT,
+    DepartmentId INT NOT NULL,
+    Salary INT NOT NULL,
+    HireDate DATETIME NOT NULL,
+    PRIMARY KEY(Id),
+    FOREIGN KEY (ManagerId) REFERENCES Employees(Id),
+    FOREIGN KEY (DepartmentId) REFERENCES Departments(Id)
+);
+
+INSERT INTO Employees
+    (Id, FName, LName, PhoneNumber, ManagerId, DepartmentId, Salary, HireDate)
+VALUES
+    (1, 'James', 'Smith', 1234567890, NULL, 1, 1000, '2001-01-01'),
+    (2, 'John', 'Johnson', 2468101214, '1', 1, 400, '2021-05-01'),
+    (3, 'Michael', 'Williams', 1357911131, '1', 2, 600, '2009-01-01'),
+    (4, 'Johnathon', 'Smith', 1212121212, '2', 1, 500, '2022-01-01')
+;
+
+CREATE TABLE Customers (
+    Id INT NOT NULL AUTO_INCREMENT,
+    FName VARCHAR(35) NOT NULL,
+    LName VARCHAR(35) NOT NULL,
+    Email varchar(100) NOT NULL,
+    PhoneNumber VARCHAR(11),
+    PreferredContact VARCHAR(5) NOT NULL,
+    PRIMARY KEY(Id)
+);
+
+INSERT INTO Customers
+    (Id, FName, LName, Email, PhoneNumber, PreferredContact)
+VALUES
+    (1, 'William', 'Jones', 'william.jones@example.com', '3347927472', 'PHONE'),
+    (2, 'David', 'Miller', 'dmiller@example.net', '2137921892', 'EMAIL'),
+    (3, 'Richard', 'Davis', 'richard0123@example.com', NULL, 'EMAIL')
+;
+
+
+CREATE TABLE Cars (
+    Id INT NOT NULL AUTO_INCREMENT,
+    CustomerId INT NOT NULL,
+    EmployeeId INT NOT NULL,
+    Model varchar(50) NOT NULL,
+    Status varchar(25) NOT NULL,
+    TotalCost INT NOT NULL,
+    PRIMARY KEY(Id),
+    FOREIGN KEY (CustomerId) REFERENCES Customers(Id),
+    FOREIGN KEY (EmployeeId) REFERENCES Employees(Id)
+);
+
+INSERT INTO Cars
+    (Id, CustomerId, EmployeeId, Model, Status, TotalCost)
+VALUES
+    ('1', '1', '2', 'Ford F-150', 'READY', '230'),
+    ('2', '1', '2', 'Ford F-150', 'READY', '200'),
+    ('3', '2', '1', 'Ford Mustang', 'WAITING', '100'),
+    ('4', '3', '3', 'Toyota Prius', 'WORKING', '1254')
+;
+
+
+-- SELECT e.LName, d.Name FROM Employees as e, Departments as d WHERE e.DepartmentId = d.Id;
+SELECT e.LName, d.Name FROM Employees as e INNER JOIN Departments as d ON e.DepartmentId = d.Id;
